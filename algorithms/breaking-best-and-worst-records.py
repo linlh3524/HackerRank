1e3611ae9d036b238dbd027f4cbd658f696e8766
lihong <2830174715@qq.com>
2023-05-31 16:59:12 +0800

    Update breaking-best-and-worst-records.py
    add personal solution.

--- algorithms/breaking-best-and-worst-records.py
+++ algorithms/breaking-best-and-worst-records.py
@@ -22,3 +22,48 @@ if __name__ == "__main__":
     print (" ".join(map(str, result)))
 
 
+    
+    #我的解决方案
+    #!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'breakingRecords' function below.
+#
+# The function is expected to return an INTEGER_ARRAY.
+# The function accepts INTEGER_ARRAY scores as parameter.
+#
+
+def breakingRecords(scores):
+    # Write your code here
+    n_min,n_max,c_max,c_min=max(scores)+1,-1,0,0
+    for i in scores:
+        if n_min>i:
+            n_min=i
+            c_min+=1
+
+        if n_max<i:
+            n_max=i
+            c_max+=1
+    return [c_min-1,c_max-1]
+
+
+if __name__ == '__main__':
+    fptr = open('d:\\a.txt', 'w')
+
+    n = int(input().strip())
+
+    scores = list(map(int, input().rstrip().split()))
+
+    result = breakingRecords(scores)
+
+    fptr.write(' '.join(map(str, result)))
+    fptr.write('\n')
+
+    fptr.close()
+
